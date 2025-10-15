52f9a2a3eb854fac60f1ba3f51b4bc69b46076bf
matiaspaco <matiaspacoviscarra@gmail.com>
2025-10-15 23:48:10 +0100

    Feature JWT - Login: Login endpoint implemented and Swagger configuration to test locally the bearer token.

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -7,6 +7,7 @@ using api.Interfaces;
 using api.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace api.Controllers
 {
@@ -17,11 +18,47 @@ namespace api.Controllers
         private readonly UserManager<AppUser> _userManager;
         private readonly ITokenService _tokenService;
 
-        public AccountController(UserManager<AppUser> userManager, ITokenService tokenService)//We initialize the constructor userManager for then implement the identity properties and funcionality
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public AccountController(UserManager<AppUser> userManager, ITokenService tokenService, SignInManager<AppUser> signInManager)//We initialize the constructor userManager for then implement the identity properties and funcionality
         {
             _userManager = userManager;
             _tokenService = tokenService;//We initialize the constructor tokenService to access
+            _signInManager = signInManager;
         }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginDto loginDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var user = await _userManager.Users.FirstOrDefaultAsync(x => x.UserName == loginDto.UserName.ToLower());
+
+            if (user == null)
+            {
+                return Unauthorized("Invalid username");
+            }
+
+            var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
+
+            if (!result.Succeeded)
+            {
+                return Unauthorized("User Name not found and/ or password incorrect.");
+            }
+
+            return Ok(new NewUserDto
+                {
+                UserName = user.UserName,
+                Email = user.Email,
+                Token = _tokenService.CreateToken(user)                    
+                }
+            );
+        }
+
+
 
         [HttpPost("register")]
         public async Task<IActionResult> Register([FromBody] RegisterDto registerDto)
